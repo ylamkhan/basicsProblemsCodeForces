094ca1fae54344cf779baa00889f9d143862c8f3
Youssef Lamkhantar <116823544+ylamkhan@users.noreply.github.com>
2024-10-27 19:59:53 +0100

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,35 +1,26 @@
 #include <iostream>
-#include <vector>
+
 
 using namespace std;
-
-vector<int> tableParticp(const vector<int>& input, int n, int k)
-{
-    vector<int> result;
-    for (int i = 0; i < n; ++i)
-    {
-        if (k + input[i] <= 5)
-            result.push_back(input[i]);
-    }
-    return result;
-}
-
-void showResult(const vector<int>& result)
-{
-    cout << result.size() / 3 << endl;
-}
-
 int main()
 {
-    int n, k;
-    vector<int> input;
-    cin >> n >> k;
-    for (int i = 0; i < n; ++i)
+    
+    int n;
+    cin>>n;
+    for(int i=0; i<n; ++i)
     {
-        int d;
-        cin >> d;
-        input.push_back(d);
+        int rating;
+        cin>>rating;
+
+        if (1900 <= rating)
+            cout<< "Division 1"<<endl;
+        else if (1600 <= rating && rating <= 1899)
+            cout<< "Division 2"<<endl;
+        else if ( 1400 <= rating && rating <= 1599)
+            cout<< "Division 3"<<endl;
+
+        else if (rating <= 1399)
+            cout<< "Division 4"<<endl;
     }
-    showResult(tableParticp(input, n, k));
     return 0;
 }
